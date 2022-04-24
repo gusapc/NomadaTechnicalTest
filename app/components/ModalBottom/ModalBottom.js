5d7bd79701ab6f8f8720b690ff4b79cae04e36cb
Gustavo Pacheco Cruz <gustavopachecocruz@Gustavos-MacBook-Pro.local>
2022-04-24 13:48:15 -0500

    ModalBottom

--- app/components/ModalBottom/ModalBottom.js
+++ app/components/ModalBottom/ModalBottom.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Modal, Text, Pressable, TouchableOpacity } from 'react-native';
+import { View, Modal, TouchableOpacity } from 'react-native';
 import styles from './ModalBottomStyle';
 
-export default function ModalBottom({ handleModal, modalVisible }) {
-	return (
-		<Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={handleModal}>
-			<View style={[styles.centeredView]}>
-				<React.Fragment>
-					<View style={styles.modalView}></View>
-					<TouchableOpacity onPress={handleModal} style={[styles.bgTouch]} />
-				</React.Fragment>
-			</View>
-		</Modal>
-	);
-}
+const ModalBottom = ({ handleModal, modalVisible, children }) => (
+	<Modal animationType="slide" transparent={true} visible={modalVisible} onRequestClose={handleModal}>
+		<View style={[styles.centeredView]}>
+			<React.Fragment>
+				<View style={styles.modalView}>{children}</View>
+				<TouchableOpacity onPress={handleModal} style={[styles.bgTouch]} />
+			</React.Fragment>
+		</View>
+	</Modal>
+);
+
+export default React.memo(ModalBottom);
 
 ModalBottom.propTypes = {
 	handleModal: PropTypes.func,
 	modalVisible: PropTypes.bool,
+	children: PropTypes.node,
 };
 
 ModalBottom.defaultProps = {
 	handleModal: () => {},
 	modalVisible: false,
+	children: null,
 };
--- app/screens/ExampleScreen/ExampleScreen.js
+++ app/screens/ExampleScreen/ExampleScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Modal, Text, Pressable, Alert } from 'react-native';
+import { View, Modal, Text, Pressable, Alert, SafeAreaView } from 'react-native';
 import styles from './ExampleScreenStyle';
 import {
 	AddCard,
@@ -15,15 +15,19 @@ import {
 } from '../../components';
 
 export default function ExampleScreen(props) {
-	const [modalVisible, setModalVisible] = useState(false);
-	const handleModal = () => setModalVisible(!modalVisible);
+	const [modalVisiblePickImg, setModalVisiblePickImg] = useState(false);
+	const handleModal = () => setModalVisiblePickImg(!modalVisiblePickImg);
 	return (
 		<View style={styles.container}>
 			<TextComponent color="greyscale900" size="xTitle" weight="bold" text={'Hey, Dev 👋'} />
 			<TextComponent color="greyscale600" size="large" weight="bold" text={'Keep up the good work!'} />
 			<TextComponent color="greyscale900" size="title" weight="bold" text={'¿Quién es el famoso?'} />
 			<AddCard onPress={handleModal} />
-			<ModalBottom handleModal={handleModal} modalVisible={modalVisible} />
+			<ModalBottom handleModal={handleModal} modalVisible={modalVisiblePickImg}>
+				<SafeAreaView>
+					<TextComponent color="greyscale900" size="xTitle" weight="bold" text={'Selecciona una foto'} />
+				</SafeAreaView>
+			</ModalBottom>
 		</View>
 	);
 }
